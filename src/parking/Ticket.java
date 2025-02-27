4d50dc3f8eeed8f82ec24f270a9c937045e2743a
Rs-845 <rosss4.47z@gmail.com>
2025-02-27 12:55:21 +0100

    feat: Creacion de clases Ubicacion y Ticket

--- src/parking/Ticket.java
+++ src/parking/Ticket.java
@@ -1,5 +1,49 @@
 package parking;
 
+import java.time.LocalDate;
+
 public class Ticket {
+    private final int id;
+    private String matricula;
+    private LocalDate fechaHoraEntrada; 
+    private Ubicacion Ubicacion;
+
+    public Ticket(int id, String matricula, LocalDate fechaHoraEntrada, Ubicacion Ubicacion) {
+        this.id = id;
+        this.matricula = matricula;
+        this.fechaHoraEntrada = fechaHoraEntrada;
+        this.Ubicacion = Ubicacion;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getFechaHoraEntrada() {
+        return fechaHoraEntrada;
+    }
+
+    public void setFechaHoraEntrada(LocalDate fechaHoraEntrada) {
+        this.fechaHoraEntrada = fechaHoraEntrada;
+    }
+
+    public Ubicacion getUbicacion() {
+        return Ubicacion;
+    }
+
+    public void setUbicacion(Ubicacion Ubicacion) {
+        this.Ubicacion = Ubicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" + "id=" + id + ", matricula=" + matricula + ", fechaHoraEntrada=" + fechaHoraEntrada + ", Ubicacion=" + Ubicacion + '}';
+    }
+    
+    
     
 }
